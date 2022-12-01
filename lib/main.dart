bf211f521898fda4e4ca1b675ccc9856e1d0ddd7
Iver Karlsbakk Småge stasj <iverks@stud.ntnu.no>
2022-12-01 01:15:09 +0100

    create functionality for custom colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -12,20 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Joulelys',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Styre lysa baby'),
     );
   }
 }
@@ -49,70 +42,230 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
+  void _rotateColors() async {
     var url = Uri.http("10.0.0.22", "/rotate");
-    await http.post(url);
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    await http.get(url);
   }
+
+  void _turnOff() async {
+    var url = Uri.http("10.0.0.22", "/off");
+    await http.get(url);
+  }
+
+  void _setRedWhite() async {
+    var url = Uri.http("10.0.0.22", "/program");
+    await http.post(url, body: "0");
+  }
+
+  void _setBlueWhite() async {
+    var url = Uri.http("10.0.0.22", "/program");
+    await http.post(url, body: "1");
+  }
+
+  void _setRotating() async {
+    var url = Uri.http("10.0.0.22", "/program");
+    await http.post(url, body: "2");
+  }
+
+  List<Color> currentColors = [Colors.yellow, Colors.green];
+  Color color1 =
+      Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  Color color2 =
+      Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  Color color3 =
+      Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  Color color4 =
+      Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  // Unit is 10ths of a second, will wrap if it exceeds 255 (luckily)
+  int delay = 20;
+
+  void _setCustom() async {
+    var url = Uri.http("10.0.0.22", "/custom");
+    await http.post(url, body: [
+      color1.red, color1.green, color1.blue, // RGB1
+      color2.red, color2.green, color2.blue, // RGB2
+      color3.red, color3.green, color3.blue, // RGB3
+      color4.red, color4.green, color4.blue, // RGB4
+      delay,
+    ]);
+  }
+
+  final bool _portraitOnly = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              margin: const EdgeInsets.all(6),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          titlePadding: const EdgeInsets.all(0),
+                          contentPadding: const EdgeInsets.all(0),
+                          content: SingleChildScrollView(
+                            child: HueRingPicker(
+                              pickerColor: color1,
+                              onColorChanged: (Color color) => setState(() {
+                                color1 = color;
+                              }),
+                              portraitOnly: _portraitOnly,
+                              enableAlpha: false,
+                              displayThumbColor: true,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  backgroundColor: color1,
+                  child: const Text("1"),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          titlePadding: const EdgeInsets.all(0),
+                          contentPadding: const EdgeInsets.all(0),
+                          content: SingleChildScrollView(
+                            child: HueRingPicker(
+                              pickerColor: color2,
+                              onColorChanged: (Color color) => setState(() {
+                                color2 = color;
+                              }),
+                              portraitOnly: _portraitOnly,
+                              enableAlpha: false,
+                              displayThumbColor: true,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  backgroundColor: color2,
+                  child: const Text("2"),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          titlePadding: const EdgeInsets.all(0),
+                          contentPadding: const EdgeInsets.all(0),
+                          content: SingleChildScrollView(
+                            child: HueRingPicker(
+                              pickerColor: color3,
+                              onColorChanged: (Color color) => setState(() {
+                                color3 = color;
+                              }),
+                              portraitOnly: _portraitOnly,
+                              enableAlpha: false,
+                              displayThumbColor: true,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  backgroundColor: color3,
+                  child: const Text("3"),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          titlePadding: const EdgeInsets.all(0),
+                          contentPadding: const EdgeInsets.all(0),
+                          content: SingleChildScrollView(
+                            child: HueRingPicker(
+                              pickerColor: color4,
+                              onColorChanged: (Color color) => setState(() {
+                                color4 = color;
+                              }),
+                              portraitOnly: _portraitOnly,
+                              enableAlpha: false,
+                              displayThumbColor: true,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  backgroundColor: color4,
+                  child: const Text("4"),
+                ),
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Slider(
+              value: (delay / 10).toDouble(),
+              min: 0.1,
+              max: 4,
+              divisions: 39,
+              label: "Tidsintervall mellom lyskonfigurasjon".toString(),
+              onChanged: (double value) =>
+                  setState(() => delay = (value * 10).round()),
+            ),
+            Container(
+              margin: const EdgeInsets.all(6),
+              child: OutlinedButton(
+                onPressed: _setCustom,
+                child: Text(
+                    "Bruk de gitte fargene med tidsintervall ${(delay * 0.1).toStringAsFixed(1)} s"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(6),
+              child: OutlinedButton(
+                onPressed: _setRedWhite,
+                child: const Text("Set to red and white"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(6),
+              child: OutlinedButton(
+                onPressed: _setBlueWhite,
+                child: const Text("Set to blue and white"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(6),
+              child: OutlinedButton(
+                onPressed: _setRotating,
+                child: const Text("Set to rotating"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(6),
+              child: ElevatedButton(
+                onPressed: _rotateColors,
+                child: const Text("Rotate colors"),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _turnOff,
+        tooltip: 'Turn off',
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.power_off_outlined),
+      ),
     );
   }
 }
